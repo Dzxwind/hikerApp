d2db34da2b8ce5d61ec8a4814dd20efba3964b94
Dzxwind <dzxwind@163.com>
2018-03-23 10:48:06 +0800

    假数据

---
+++ static/data/random.js
@@ -0,0 +1,43 @@
+[
+  {
+    "repeat(30)": {
+      user_id: "{{objectId()}}",
+      user_index: "{{index()}}",
+      user_name: "{{surname()}}",
+      user_img: function() {
+        return "../images/headImg" + this.user_index + ".jpg";
+      },
+      user_sex: '{{random("男","女")}}',
+      user_sign: "{{lorem('sentences')}}",
+      way_index: [{ "repeat(0,8)": "{{integer(0,20)}}" }],
+      article_index: [{ "repeat(0,2)": "{{integer(0,20)}}" }]
+    }
+  },
+  {
+    "repeat(20)": {
+      article_id: "{{objectId()}}",
+      article_index: "{{index()}}",
+      article_name: "{{lorem(8,'words')}}",
+      article_img: function() {
+        return "../images/articleImg" + this.article_index + ".jpg";
+      },
+      article_content: "{{lorem(1,'paragraphs')}}",
+      user_index: "{{integer(30)}}",
+      way_index: "{{integer(20)}}"
+    }
+  },
+  {
+    "repeat(20)": {
+      way_id: "{{objectId()}}",
+      way_index: "{{index()}}",
+      way_name: "{{lorem(2,'words')}}",
+      way_subname:"{{city()}}",
+      way_img: function() {
+        return "../images/wayImg" + this.way_index + ".jpg";
+      },
+      way_intro: "{{lorem(1,'paragraphs')}}",
+      user_index:  [{ "repeat(0,5)": "{{integer(0,30)}}" }],
+      article_index: [{ "repeat(0,3)": "{{integer(0,20)}}" }]
+    }
+  }
+];
