2c2712f60066c6b54680887e3f80c517e271f454
Dzxwind <dzxwind@163.com>
2018-04-14 14:43:55 +0800

    way数据导入完成，aritcle数据联动功能待添加，routerview命名视图有问题

--- src/components/pages/way.vue
+++ src/components/pages/way.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="way">
     <subBanner :useWhichBanner="whichBanner"></subBanner>
-    <div class="wayWrapper col-lg-8 col-lg-offset-2 col-xs-12">
+    <div class="wayWrapper col-lg-8 col-lg-offset-2 col-xs-12" v-if="!isShow">
       <div class="hotRoad">
         <div class="hotTitle">热门路线</div>
         <div class="hotContent">
-          <div class="hotContent_i" v-for="(item,index) in topShow" :key="item.name" v-if="index < 2">
+          <div class="hotContent_i" v-for="(item,index) in topShow" :key="item.name" v-if="index < 2" @click="linkDetail(item)">
             <div class="hotContentName">{{item.name}}<br>{{item.enName}}</div>
             <div class="hotNumber">{{item.people}}人去过</div>
             <div class="hotImg">
@@ -29,6 +29,7 @@
         </el-tabs>
       </div>
     </div>
+    <router-view v-if="isShow"></router-view>
   </div>
 </template>
 <style lang="scss">
@@ -69,20 +70,26 @@ export default {
           name: "香港",
           enName: "HongKong",
           people: "584",
-          topImage: require("./../../assets/hotImg1.png")
+          topImage: require("./../../assets/hotImg1.png"),
+          link:"10"
         },
         {
           name: "普罗旺斯",
           enName: "Provence",
           people: "617",
-          topImage: require("./../../assets/hotImg2.png")
+          topImage: require("./../../assets/hotImg2.png"),
+          link:"4"
         }
       ],
-      activeName: '0'
+      activeName: '0',
+      isShow:false
     };
   },
   methods: {
-
+    linkDetail(item){
+      this.isShow = true
+      this.$router.push({path:`/way/waydetail/${item.link}`});
+    }
   },
   components: {
     subBanner,
--- src/components/pages/waydetail.vue
+++ src/components/pages/waydetail.vue
@@ -1,11 +1,96 @@
 <template>
-  
+  <div class="waydetail">
+    <div class="waydetailWrapper col-xs-12 col-lg-8 col-lg-offset-2">
+      <div class="titleBox">
+        <div class="title">
+          {{wayIntro.way_name}}
+        </div>
+        <div class="subTitle">          
+          <span class="locate">
+            <img src="./../../assets/locate.png" alt="">雅典
+          </span>
+          <span class="read">浏览：117</span>
+        </div>
+      </div>
+      <div class="hr"></div>
+      <div class="imageBox">
+        <img :src="wayIntro.way_img" alt="">
+      </div>
+      <div class="contentBox">
+        {{wayIntro.way_intro}}
+        <div class="userBox">
+          <div class="user_i" v-for="(item,index) in userInfo" :key="item.user_index" v-if="index < 4" :style="{backgroundImage:'url(' + item.user_img + ')'}" @click="linkUser(item)"></div>
+          <span>等{{userInfo.length}}人去过</span>
+        </div>
+      </div>
+      <!-- 文章联动功能待添加 -->
+      <!-- <div class="articleBox">
+        <div class="articleBox_i" v-for="item in articleInfo" :key="item.article_index">
+          <img :src="item.article_img" alt="">
+        </div>
+      </div> -->
+    </div>
+  </div>
 </template>
 <style lang="scss">
-
+@import "../../assets/scss/defined.scss";
+@import "../../assets/scss/pages/waydetail.scss";
 </style>
 <script>
 export default {
-  name:'waydetail',
-}
+  name: "waydetail",
+  computed: {
+    userData() {
+      return this.$store.state.userData;
+    },
+    wayData() {
+      return this.$store.state.wayData;
+    },
+    articleData() {
+      return this.$store.state.articleData;
+    },
+    wayIntro() {
+      for (let i = 0; i < this.wayData.length; i++) {
+        if (this.$route.params.wayIndex == this.wayData[i].way_index) {
+          return this.wayData[i];
+        }
+      }
+    },
+    userInfo() {
+      let tmpDatas = [];
+      for (let j = 0; j < this.userData.length; j++) {
+        for (let i = 0; i < this.userData[j].way_index.length; i++) {
+          if (this.$route.params.wayIndex == this.userData[j].way_index[i]) {
+            tmpDatas.push(this.userData[j]);
+          }
+        }
+      }
+      return tmpDatas;
+    },
+    articleInfo(){
+      let tmpDatas2 = [];
+      for (let j = 0; j < this.articleData.length; j++) {
+          if (this.$route.params.wayIndex == this.articleData[j].way_index) {
+            tmpDatas2.push(this.articleData[j]);
+        }        
+      }
+      if (tmpDatas2.length != 0) {
+        return tmpDatas2;
+      }else if (tmpDatas2.length == 0){
+        tmpDatas2 = {
+          article_name:"暂无数据"
+        }
+        return tmpDatas2;
+      }
+    }
+  },
+  methods:{
+    linkUser(item){
+      this.$router.push({path:`/find/finddetail/${item.user_index}`})
+    },
+    linkarticle(item){
+      this.$router.push({path:`/article/articledetail/${item.user_index}`})
+    }
+  }
+};
 </script>
